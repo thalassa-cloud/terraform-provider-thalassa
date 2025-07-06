d5a28a348f3111297e1982b985bbf5a3d190575d
Thomas Kooi <thomas@thalassa.cloud>
2025-07-06 20:23:37 +0200

    refactor(iaas): change cloud init template resource with improved error handling and add descriptions to schema fields

--- thalassa/iaas/resource_cloud_init_template.go
+++ thalassa/iaas/resource_cloud_init_template.go
@@ -2,10 +2,15 @@ package iaas
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	iaas "github.com/thalassa-cloud/client-go/iaas"
+	tcclient "github.com/thalassa-cloud/client-go/pkg/client"
+
+	"github.com/thalassa-cloud/terraform-provider-thalassa/thalassa/convert"
 	"github.com/thalassa-cloud/terraform-provider-thalassa/thalassa/provider"
 )
 
@@ -17,12 +22,14 @@ func resourceCloudInitTemplate() *schema.Resource {
 		DeleteContext: resourceCloudInitTemplateDelete,
 		Schema: map[string]*schema.Schema{
 			"id": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The identity of the cloud init template",
 			},
 			"slug": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The slug of the cloud init template",
 			},
 			"organisation_id": {
 				Type:        schema.TypeString,
@@ -31,24 +38,28 @@ func resourceCloudInitTemplate() *schema.Resource {
 				Description: "Reference to the Organisation of the Machine Type. If not provided, the organisation of the (Terraform) provider will be used.",
 			},
 			"labels": {
-				Type:     schema.TypeMap,
-				Optional: true,
-				ForceNew: true,
+				Type:        schema.TypeMap,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Labels to add to the cloud init template",
 			},
 			"annotations": {
-				Type:     schema.TypeMap,
-				Optional: true,
-				ForceNew: true,
+				Type:        schema.TypeMap,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Annotations to add to the cloud init template",
 			},
 			"name": {
-				Type:     schema.TypeString,
-				ForceNew: true,
-				Required: true,
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "The name of the cloud init template",
 			},
 			"content": {
-				Type:     schema.TypeString,
-				ForceNew: true,
-				Required: true,
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "The content of the cloud init template",
 			},
 		},
 	}
@@ -58,7 +69,7 @@ func resourceCloudInitTemplate() *schema.Resource {
 func resourceCloudInitTemplateCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client, err := provider.GetClient(provider.GetProvider(m), d)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf("error creating client: %s", err))
 	}
 
 	create := iaas.CreateCloudInitTemplateRequest{
@@ -67,15 +78,15 @@ func resourceCloudInitTemplateCreate(ctx context.Context, d *schema.ResourceData
 	}
 
 	if labels, ok := d.GetOk("labels"); ok {
-		create.Labels = convertLabels(labels.(map[string]interface{}))
+		create.Labels = convert.ConvertToMap(labels.(map[string]interface{}))
 	}
 	if annotations, ok := d.GetOk("annotations"); ok {
-		create.Annotations = convertAnnotations(annotations.(map[string]interface{}))
+		create.Annotations = convert.ConvertToMap(annotations.(map[string]interface{}))
 	}
 
 	cloudInitTemplate, err := client.IaaS().CreateCloudInitTemplate(ctx, create)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf("error creating cloud init template: %s", err))
 	}
 
 	d.SetId(cloudInitTemplate.Identity)
@@ -95,12 +106,16 @@ func resourceCloudInitTemplateCreate(ctx context.Context, d *schema.ResourceData
 func resourceCloudInitTemplateRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client, err := provider.GetClient(provider.GetProvider(m), d)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf("error creating client: %s", err))
 	}
 
 	cloudInitTemplate, err := client.IaaS().GetCloudInitTemplate(ctx, d.Id())
 	if err != nil {
-		return diag.FromErr(err)
+		if tcclient.IsNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(fmt.Errorf("error reading cloud init template: %s", err))
 	}
 
 	d.SetId(cloudInitTemplate.Identity)
@@ -119,30 +134,18 @@ func resourceCloudInitTemplateRead(ctx context.Context, d *schema.ResourceData, 
 func resourceCloudInitTemplateDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client, err := provider.GetClient(provider.GetProvider(m), d)
 	if err != nil {
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf("error creating client: %s", err))
 	}
 
 	err = client.IaaS().DeleteCloudInitTemplate(ctx, d.Id())
 	if err != nil {
-		return diag.FromErr(err)
+		if tcclient.IsNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(fmt.Errorf("error deleting cloud init template: %s", err))
 	}
 
 	d.SetId("")
 	return nil
-}
-
-func convertLabels(labels map[string]interface{}) map[string]string {
-	convertedLabels := make(map[string]string)
-	for key, value := range labels {
-		convertedLabels[key] = value.(string)
-	}
-	return convertedLabels
-}
-
-func convertAnnotations(annotations map[string]interface{}) map[string]string {
-	convertedAnnotations := make(map[string]string)
-	for key, value := range annotations {
-		convertedAnnotations[key] = value.(string)
-	}
-	return convertedAnnotations
 }
