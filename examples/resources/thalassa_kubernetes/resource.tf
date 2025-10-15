3c48c052c1d6b0c14cb5edc2f5da0d3ed1a00f61
Thomas Kooi <thomas@thalassa.cloud>
2025-10-15 22:01:28 +0200

    feat(docs): improved Kubernetes cluster and node pool documentation
    - Added detailed sections on cluster types, important notes, and security configuration for Kubernetes clusters.
    - Introduced autoscaling configuration guidelines for Kubernetes node pools, clarifying requirements for enabling and disabling autoscaling.
    - Updated example configurations to reflect new attributes and best practices.

--- examples/resources/thalassa_kubernetes/resource.tf
+++ examples/resources/thalassa_kubernetes/resource.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    thalassa = {
-      source = "thalassa.cloud/thalassa/thalassa"
-    }
-  }
-}
-
-provider "thalassa" {
-  # Configuration options
-}
-
 # Create a VPC for the Kubernetes cluster
 resource "thalassa_vpc" "example" {
   name        = "example-vpc"
@@ -31,20 +19,24 @@ resource "thalassa_kubernetes_cluster" "example" {
   name        = "example-kubernetes-cluster"
   description = "Example Kubernetes cluster"
   region      = "nl-01"
-  # version     = "1.28"
-  cluster_version = "1.33"
-  api_server_acls = {
-    allowed_cidrs = ["10.0.0.0/16"]
+  subnet_id   = thalassa_subnet.example.id
+  api_server_acls {
+    allowed_cidrs = ["10.0.0.0/16", "178.85.83.115/32"]
   }
 }
 
 # Create a Kubernetes node pool with Thalassa default values
 resource "thalassa_kubernetes_node_pool" "example" {
-  kubernetes_cluster_id = thalassa_kubernetes_cluster.example.id
   name                  = "example-node-pool"
-  machine_type          = "pgp-small" # Available: pgp-small, pgp-medium, pgp-large, pgp-xlarge, pgp-2xlarge, pgp-4xlarge, dgp-small, dgp-medium, dgp-large, dgp-xlarge, dgp-2xlarge, dgp-4xlarge
-  machine_image         = "ubuntu-22.04"
-  node_count            = 0
+  cluster_id = thalassa_kubernetes_cluster.example.id
+  subnet_id = thalassa_subnet.example.id
+  availability_zone     = "nl-01a"
+  machine_type          = "pgp-small"
+  # replicas              = 2
+  enable_autoscaling    = true
+  min_replicas          = 1
+  max_replicas          = 2
+  # kubernetes_version    = "1-34-1"
 }
 
 # Output the Kubernetes cluster details
@@ -56,7 +48,7 @@ output "kubernetes_cluster_name" {
   value = thalassa_kubernetes_cluster.example.name
 }
 
-# Output the node pool details
+# # Output the node pool details
 output "node_pool_id" {
   value = thalassa_kubernetes_node_pool.example.id
 }
